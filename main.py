ddc30262500f4aaedf3ce2dd98e88ccb0d60a53f
mizjogit <git@mizjo.com>
2016-12-17 07:42:31 +1100

    deep sleep, socket error handling

--- main.py
+++ main.py
@@ -2,23 +2,20 @@ import dht
 import machine
 import time
 import socket
+import ubinascii
 #import ssd1306
 import network
 from machine import I2C, Pin
 sensor = dht.DHT22(Pin(2))
 
 ##########################################################
-deviceID='003'
 SSID = 'yai'
 PASSWORD = 'schumacher14'
+INTERVAL=30000
 ##########################################################
-
-
-
-
 #i2c = I2C(Pin(5), Pin(4))
 #display = ssd1306.SSD1306_I2C(64, 48, i2c)
-
+led = Pin(2, Pin.OUT)
 rtc = machine.RTC()
 rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
 if machine.reset_cause() == machine.DEEPSLEEP_RESET:
@@ -35,14 +32,17 @@ if not sta_if.isconnected():
         pass
 print('Network configuration:', sta_if.ifconfig())
 
+mac=ubinascii.hexlify(sta_if.config('mac'),':').decode()
+
 while True:
 	addr=socket.getaddrinfo("vps.mizjo.com",8011)[0][-1]
 	s=socket.socket()
-
 	try:
 		s.connect(addr)
 	except OSError:
 		print('Connect Exception, rebooting!')
+		led.low()
+		time.sleep(5)
 		machine.reset()
 
 	print('Socket Open to vps.mizjo.com')
@@ -56,10 +56,10 @@ while True:
 	#display.show()
 	#display.text("H:"+hum,1,20)
 	#display.show()
-	data= 'Device='+deviceID+',Temp='+temp+',Humidity='+hum+'\r\n'
+	data= 'MAC='+mac+',Temp='+temp+',Humidity='+hum+'\r\n'
 	print(data)
 	s.send(data)
 	print('Going into deep sleep!')
 	time.sleep(5)
-	rtc.alarm(rtc.ALARM0, 10000)
+	rtc.alarm(rtc.ALARM0, INTERVAL)
 	machine.deepsleep()
