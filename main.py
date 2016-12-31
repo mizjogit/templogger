646ce0d14c471317425c14bbee1f0675b7875968
mizjogit <git@mizjo.com>
2017-01-01 10:36:08 +1100

    stable

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import machine
 import time
 import socket
 import ubinascii
-#import ssd1306
 import network
-from machine import I2C, Pin
+from machine import Pin	
+from time import sleep_ms
+
 sensor = dht.DHT22(Pin(2))
 
 ##########################################################
@@ -14,54 +15,75 @@ PASSWORD = 'schumacher14'
 INTERVAL=60000
 ##########################################################
 
-#i2c = I2C(Pin(5), Pin(4))
-#display = ssd1306.SSD1306_I2C(64, 48, i2c)
-#led = Pin(2, Pin.OUT)
 rtc = machine.RTC()
 rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
-if machine.reset_cause() == machine.DEEPSLEEP_RESET:
-    print('woke from a deep sleep')
-else:
-    print('power on or hard reset')
+led = Pin(2, Pin.OUT)
 
-sta_if = network.WLAN(network.STA_IF)
-if not sta_if.isconnected():
-    print('connecting to network...')
-    sta_if.active(True)
-    sta_if.connect(SSID, PASSWORD)
-    while not sta_if.isconnected():
-        pass
-print('Network configuration:', sta_if.ifconfig())
+def led_flash(count,waiton,waitoff):
+	for i in range(count):
+		led.low()
+		time.sleep_ms(waiton)
+		led.high()
+		time.sleep_ms(waitoff)
+	return
 
-mac=ubinascii.hexlify(sta_if.config('mac'),'-').decode()
+def connect_network():
+	led.low()
+	sta_if = network.WLAN(network.STA_IF)
+	sta_if.active(True)
+	sta_if.connect(SSID, PASSWORD)
+	attempts=0
+	print('NET: Connecting to network...')
+	while not sta_if.isconnected():
+	    attempts += 1
+	    time.sleep_ms(1000)
+	    print('..')
+	print('NET: Connected:', sta_if.ifconfig())
+	led.high()
+	mac=ubinascii.hexlify(sta_if.config('mac'),'-').decode()
+	return mac
 
-while True:
-	addr=socket.getaddrinfo("vps.mizjo.com",8011)[0][-1]
-	s=socket.socket()
-	try:
-		s.connect(addr)
-	except OSError:
-		print('Connect Exception, rebooting!')
-		#led.low()
-		#time.sleep(5)
-		machine.reset()
+def main():
+	if machine.reset_cause() == machine.DEEPSLEEP_RESET:
+	    print('SYS: Woke from a deep sleep')
+	else:
+	    print('SYS: Power on or hard reset')
 
-	print('Socket Open to vps.mizjo.com')
-	print('Measuring DHT')
-	sensor.measure()
-	#display.fill(0)
-	#display.show()
-	temp='%d.1' % (sensor.temperature())
-	hum='%d.1' % (sensor.humidity())
-	#display.text("T:"+temp,1,10)
-	#display.show()
-	#display.text("H:"+hum,1,20)
-	#display.show()
-	data= 'MAC='+mac+',Temp='+temp+',Humidity='+hum+'\r\n'
-	#data= 'Temp='+temp+',Humidity='+hum+',MAC='+mac+'\r\n'
-	print(data)
-	s.send(data)
-	print('Going into deep sleep!')
-	time.sleep(5)
-	rtc.alarm(rtc.ALARM0, INTERVAL)
-	machine.deepsleep()
+	mac=connect_network();
+
+	while True:
+		try:
+			addr=socket.getaddrinfo("vps.mizjo.com",8011)[0][-1]
+		except OSError:
+			print('SOCK: getaddress Fail!')
+			led_flash(3,100,50)
+			machine.reset()
+		try:
+			s=socket.socket()
+		except OSError:
+			print('SOCK: Create Fail!')
+			led_flash(5,100,50)
+			machine.reset()
+		try:
+			s.connect(addr)
+		except OSError:
+			print('SOCK: Connect Fail!')
+			led_flash(7,100,50)
+			machine.reset()
+		
+		print('SOCK: Socket Open to vps.mizjo.com')
+		time.sleep(1)
+		sensor.measure()
+		temp='%d.1' % (sensor.temperature())
+		hum='%d.1' % (sensor.humidity())
+		print('DHT: Measurement complete')
+		data= 'MAC='+mac+',Temp='+temp+',Humidity='+hum+'\r\n'
+		print(data)
+		s.send(data)
+		print('SYS: Going into deep sleep!')
+		time.sleep(1)
+		rtc.alarm(rtc.ALARM0, INTERVAL)
+		machine.deepsleep()
+
+if __name__ == '__main__':
+		main()
